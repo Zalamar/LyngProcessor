3aa11fca89e83c310f03d7aa80217232f9cdc4c9
Tobias Rydberg <zalamar@protonmail.com>
2021-04-10 16:25:46 +0200

    Add sign extender

---
+++ src/main/scala/lyng/execute/extender.scala
@@ -0,0 +1,41 @@
+package lyng.execute
+
+import chisel3._
+import chisel3.util._
+
+class extender extends Module {
+  val io = IO(new Bundle {
+    val immediate_in = Input(Bits(11.W))
+    val ext_mode = Input(Bits(3.W))
+    val immediate_out = Output(Bits(16.W))
+  })
+
+  // See ALU_Control_Codes for opcodes
+  // TODO: Could this be improved by doing it in steps?
+  when(io.ext_mode === 0.U) {
+    io.immediate_out := Cat("b00000000000".U, io.immediate_in)
+  } .elsewhen(io.ext_mode === 1.U) {
+    when (io.immediate_in(4) === 1.U) {
+      io.immediate_out := Cat("b11111111111".U, io.immediate_in)
+    } .otherwise {
+      io.immediate_out := Cat("b00000000000".U, io.immediate_in)
+    }
+  } .elsewhen(io.ext_mode === 2.U) {
+    io.immediate_out := Cat("b00000000".U, io.immediate_in)
+  } .elsewhen(io.ext_mode === 3.U) {
+    when (io.immediate_in(7) === 1.U) {
+      io.immediate_out := Cat("b11111111".U, io.immediate_in)
+    } .otherwise {
+      io.immediate_out := Cat("b00000000".U, io.immediate_in)
+    }
+  } .elsewhen(io.ext_mode === 4.U) {
+    io.immediate_out := Cat("b00000".U, io.immediate_in)
+  } .elsewhen(io.ext_mode === 5.U) {
+    when (io.immediate_in(10) === 1.U) {
+      io.immediate_out := Cat("b11111".U, io.immediate_in)
+    } .otherwise {
+      io.immediate_out := Cat("b00000".U, io.immediate_in)
+    }
+  }
+
+}
