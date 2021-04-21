d1ba136b50d29f06ebbcad9ea2065905a2667113
Dario Passarello <dario.passarello@gmail.com>
2021-04-21 11:39:48 +0200

    test: upgraded simulation

--- src/test/scala/lyng/Simulation.scala
+++ src/test/scala/lyng/Simulation.scala
@@ -9,21 +9,28 @@ import chisel3.iotesters.Driver
 class LyngTopSimulator(dut: LyngTop) extends PeekPokeTester(dut) {
     val instructions = List(
         0x1922,   //ADDI $1, $1, 2
-        0x1922     //ADDI $1, $1, 2
+        0x1922    //ADDI $1, $1, 2
     )
 
     //Load
 
-    poke(dut.io.load, 2)
+    poke(dut.io.load, 1)
     for((instr, addr) <- instructions.zipWithIndex) {
-        poke(dut.io.addr, addr)
+        poke(dut.io.addr, addr << 1)
         poke(dut.io.value, instr)
         step(1)
     }
     step(1)
-    //Execute
     poke(dut.io.load, 0)
-    step(10)
+    //Execute
+    var end = false
+    for(i <- 1 to 15) {
+        val valid = peek(dut.io.out_valid)
+        if(valid == 1) {
+            println(peek(dut.io.out).toString)
+        }
+        step(1)
+    }
 }   
 
 
