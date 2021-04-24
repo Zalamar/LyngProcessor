008ef4898bb5fa552d583665554d623af0551a87
Tobias Rydberg <zalamar@protonmail.com>
2021-04-24 11:17:53 +0200

    Fix extender length

--- src/main/scala/lyng/execute/Extender.scala
+++ src/main/scala/lyng/execute/Extender.scala
@@ -12,30 +12,30 @@ class Extender extends Module {
 
   // See ALU_Control_Codes for opcodes
   // TODO: Could this be improved by doing it in steps?
-  io.immediate_out := Cat("b00000000000".U, io.immediate_in)
+  io.immediate_out := Cat("b00000000000".U, io.immediate_in(4,0))
   when(io.ext_mode === 0.U) {
-    io.immediate_out := Cat("b00000000000".U, io.immediate_in)
+    io.immediate_out := Cat("b00000000000".U, io.immediate_in(4,0))
   } .elsewhen(io.ext_mode === 1.U) {
     when (io.immediate_in(4) === 1.U) {
-      io.immediate_out := Cat("b11111111111".U, io.immediate_in)
+      io.immediate_out := Cat("b11111111111".U, io.immediate_in(4,0))
     } .otherwise {
-      io.immediate_out := Cat("b00000000000".U, io.immediate_in)
+      io.immediate_out := Cat("b00000000000".U, io.immediate_in(4,0))
     }
   } .elsewhen(io.ext_mode === 2.U) {
-    io.immediate_out := Cat("b00000000".U, io.immediate_in)
+    io.immediate_out := Cat("b00000000".U, io.immediate_in(7,0))
   } .elsewhen(io.ext_mode === 3.U) {
     when (io.immediate_in(7) === 1.U) {
-      io.immediate_out := Cat("b11111111".U, io.immediate_in)
+      io.immediate_out := Cat("b11111111".U, io.immediate_in(7,0))
     } .otherwise {
-      io.immediate_out := Cat("b00000000".U, io.immediate_in)
+      io.immediate_out := Cat("b00000000".U, io.immediate_in(7,0))
     }
   } .elsewhen(io.ext_mode === 4.U) {
-    io.immediate_out := Cat("b00000".U, io.immediate_in)
+    io.immediate_out := Cat("b00000".U, io.immediate_in(10,0))
   } .otherwise {
     when (io.immediate_in(10) === 1.U) {
-      io.immediate_out := Cat("b11111".U, io.immediate_in)
+      io.immediate_out := Cat("b11111".U, io.immediate_in(10,0))
     } .otherwise {
-      io.immediate_out := Cat("b00000".U, io.immediate_in)
+      io.immediate_out := Cat("b00000".U, io.immediate_in(10,0))
     }
   }
 
